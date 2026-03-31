f107e63f6b84099d7908ced7ec033ea9816966de
Shukakun91 <kalleloof@gmail.com>
2026-03-31 14:10:12 +0200

    Fixed health bars?

--- Assets/Scripts/UI/EnemyHealthBar.cs
+++ Assets/Scripts/UI/EnemyHealthBar.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class EnemyHealthBar : MonoBehaviour
 {
-    [SerializeField] Transform foregroundBar;
+    [SerializeField] Transform bgBar;
+    [SerializeField] Transform fgBar;
     [SerializeField] TextMeshProUGUI healthText;
 
     private EnemyStats enemyStats;
@@ -19,9 +20,14 @@ public class EnemyHealthBar : MonoBehaviour
         transform.rotation = cam.transform.rotation; //Rotates the health bar to always face the camera
 
         if (enemyStats.MaxHealth > 0)
-        {
+        { 
             float fill = enemyStats.Health / enemyStats.MaxHealth;
-            foregroundBar.localScale = new Vector3(fill, 1, 1);
+
+            RectTransform fgRec = fgBar.GetComponent<RectTransform>();
+            RectTransform bgRec = bgBar.GetComponent<RectTransform>();
+
+            fgRec.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, bgRec.rect.width * fill);
+
             healthText.text = Mathf.Ceil(enemyStats.Health).ToString();
         }
     }
