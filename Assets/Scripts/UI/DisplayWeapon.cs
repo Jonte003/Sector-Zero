194625406b8657c048943f3ebf7c6a74900354b9
Shukakun91 <kalleloof@gmail.com>
2026-04-28 19:45:46 +0200

    Worked on the loadout lobby

--- Assets/Scripts/UI/DisplayWeapon.cs
+++ Assets/Scripts/UI/DisplayWeapon.cs
@@ -1,15 +1,28 @@
+using TMPro;
 using UnityEngine;
 
 public class DisplayWeapon : MonoBehaviour
 {
     [SerializeField] float rotationSpeed;
+    [SerializeField] string weaponName;
+    [SerializeField] TextMeshPro displayText;
+    GunSettings thisGun;
     void Start()
     {
-        //get model here later
+        if (weaponName == "Assault Rifle") { thisGun = GunSettings.AssaultRifle; }
+        else if (weaponName == "Burst Rifle") { thisGun = GunSettings.BurstRifle; }
+        else if (weaponName == "Shotgun") { thisGun = GunSettings.Shotgun; }
+        else if (weaponName == "Pistol") { thisGun = GunSettings.Pistol; }
+        else if (weaponName == "Smg") { thisGun = GunSettings.Smg; }
+        else if (weaponName == "Revolver") { thisGun = GunSettings.Revolver; }
+        else Debug.LogError("Invalid weapon name for display weapon: " + weaponName);
+
+
+        displayText.text = thisGun.GunName;
     }
 
     void Update()
     {
-        transform.Rotate(Vector3.fwd, rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
     }
 }
